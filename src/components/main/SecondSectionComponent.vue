11c23b51b918a71b7b65bde65502c21d8f394bd0
Nazar <nzrsdv17@gmail.com>
2025-04-22 22:41:51 +0500

    finale day 2

--- src/components/main/SecondSectionComponent.vue
+++ src/components/main/SecondSectionComponent.vue
@@ -2,17 +2,15 @@
 import WineCard from "../../UI/cards/WineCard.vue";
 </script>
 <template>
-  <section class="w-full h-160">
-    <h2 class="">Наш ассортимент</h2>
-    <div class="h-[1px] w-full bg-main-2">
-      <WineCard
-        :info="{
-          wineColor: 'red',
-          wineName: 'Красное вино',
-          winePlace: 'Франция, Прованс',
-          winePrice: 200,
-        }"
-      />
+  <section
+    class="w-full h-160 flex flex-col items-center justify-center gap-20"
+  >
+    <div class="flex flex-col w-full">
+      <h2 class="">Наш ассортимент</h2>
+      <div class="h-[1px] w-full bg-main-2"></div>
+    </div>
+    <div class="flex flex-row items-center justify-center gap-21">
+      <WineCard v-for="(item, index) in wineCards" :key="index" :info="item" />
     </div>
   </section>
 </template>
@@ -20,5 +18,29 @@ import WineCard from "../../UI/cards/WineCard.vue";
 export default {
   name: "SecondSectionComponent",
   components: { WineCard },
+  data() {
+    return {
+      wineCards: [
+        {
+          wineColor: "red",
+          wineName: "Красное вино",
+          winePlace: "Франция, Прованс",
+          winePrice: 200,
+        },
+        {
+          wineColor: "white",
+          wineName: "Красное вино",
+          winePlace: "Франция, Прованс",
+          winePrice: 200,
+        },
+        {
+          wineColor: "red",
+          wineName: "Красное вино",
+          winePlace: "Франция, Прованс",
+          winePrice: 200,
+        }
+      ],
+    };
+  },
 };
 </script>
