1f0fbe8a00d7fd0c1efaad9a7c6929be3e21e858
Nazar <nzrsdv17@gmail.com>
2025-04-30 22:29:13 +0500

    second section new design no response: card's bug fix of stars container styles (responsive a litle)

--- src/UI/cards/WineCard.vue
+++ src/UI/cards/WineCard.vue
@@ -1,42 +1,76 @@
 <script setup lang="ts">
 import RedWhine from "../../assets/img/RedWine_photo.png";
 import WhiteWine from "../../assets/img/WhiteWine_photo.png";
+
+import ActiveStar from "../../assets/icons/Star_active_icon.svg";
+import DisabledStar from "../../assets/icons/Star_disabled_icon.svg";
+
+import ActiveHeart from "../../assets/icons/Heart_active_icon.svg";
 </script>
 
 <template>
-  <div class="relative pt-40 w-80">
-    <div class="w-15 h-64 absolute top-0 left-[50%] -translate-x-[50%] z-10">
-      <img v-if="info.wineColor == 'red'" :src="RedWhine" alt="" />
-      <img v-if="info.wineColor == 'white'" :src="WhiteWine" alt="" />
-    </div>
+  <router-link to="">
     <div
       :class="[
-        'relative w-full h-45 rounded-4xl flex flex-col items-center justify-center gap-7 font-main z-20',
-        info.wineColor == 'white'
-          ? 'bg-accent-2 text-accent-15'
-          : 'bg-accent-1 text-main-2',
+        'w-88 h-98 flex flex-col rounded-md font-accent relative pt-10',
+        info.type == 'red' || info.type == 'rose' ? 'bg-main-2' : 'bg-main-3',
       ]"
     >
-      <div class="flex flex-col items-center justify-center gap">
-        <h4 class="font-semibold text-3xl">{{ info.wineName }}</h4>
-        <p class="opacity-60 text-lg">{{ info.winePlace }}</p>
+      <div
+        class="absolute size-8 right-1 top-2 flex items-center justify-center duration-200 hover:scale-110 cursor-pointer"
+      >
+        <img :src="ActiveHeart" alt="" />
       </div>
       <div
-        :class="[
-          'border-[2px] border-solid rounded-lg px-5 py-1 text-xl cursor-pointer duration-200 hover:scale-110',
-          info?.wineColor == 'white' ? 'border-accent-15' : ' border-main-2',
-        ]"
+        class="flex h-full flex-col items-center justify-between text-main-05"
       >
-        {{ info?.winePrice }}$
+        <div class="w-12 h-50">
+          <img
+            :src="
+              info.type == 'red' || info.type == 'rose' ? RedWhine : WhiteWine
+            "
+            alt=""
+          />
+        </div>
+        <div class="w-full flex flex-col items-start justify-center pl-6 pb-6">
+          <h3>{{ info.wine }}</h3>
+          <p class="font-semibold text-lg">${{ info.price }}</p>
+          <div class="flex items-center justify-center gap-2">
+            <div class="flex items-center justify-center">
+              <div
+                class="size-4"
+                v-for="(star, index) in ratingStars"
+                :key="index"
+              >
+                <img :src="star ? ActiveStar : DisabledStar" alt="" />
+              </div>
+            </div>
+            {{ info.rating.average }}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
 <script lang="ts">
 export default {
   name: "WineCard",
   props: {
     info: { type: Object, required: true },
+  },
+  data() {
+    return {
+      ratingStars: [0, 0, 0, 0, 0],
+    };
+  },
+  created() {
+    this.ratingStars.forEach((element, index) => {
+      if (Math.round(Number(this.info.rating.average)) >= index + 1) {
+        this.ratingStars[index] = 1;
+      } else {
+        this.ratingStars[index] = 0;
+      }
+    });
   },
 };
 </script>
