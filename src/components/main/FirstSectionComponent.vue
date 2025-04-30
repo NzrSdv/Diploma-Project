65c1923b5383d81b3078dbc666d22b03ec9b29ab
Nazar <nzrsdv17@gmail.com>
2025-04-30 20:55:47 +0500

    first section new design no responsive

--- src/components/main/FirstSectionComponent.vue
+++ src/components/main/FirstSectionComponent.vue
@@ -5,16 +5,19 @@ import Wines_bg from "../../assets/img/Wines_1_photo.jpg";
 <template>
   <section
     :style="[`background-image:url(${Wines_bg})`]"
-    class="w-full h-160 bg-cover bg-center"
+    class="container w-full h-220 bg-cover bg-bottom"
   >
-    <div class="w-full h-full bg-linear-to-l from-black/10 to-black/60">
-      <div class="px-31 h-full flex flex-col items-start justify-center gap-2">
-        <h1 class="max-w-xl text-main font-bold text-7xl">
+    <div class="w-full h-full flex items-center justify-center bg-linear-to-l from-black/10 to-black/60">
+      <div class="max-w-3xl h-full flex flex-col items-center justify-start gap-3 text-center pt-32">
+        <h1 class=" text-main font-bold text-8xl">
           Порадуйте себя и гостей
         </h1>
-        <p class="max-w-2xl text-3xl">
-          Здесь тщательно подобранная коллекция вин, которая украсит любое
-          торжество.
+        <p class="max-w-3xl text-xl flex flex-col">
+          <span class="font-semibold text-2xl">Твое идеальное вино — на расстоянии одного клика.</span>
+           Красное, белое,
+          розовое — у нас есть то, что нужно именно вам. Легкий заказ, быстрая
+          доставка и только проверенные винодельни. Откройте бутылку счастья уже
+          сегодня!
         </p>
       </div>
     </div>
